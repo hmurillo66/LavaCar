fe6de1fa957c62d3a299f98445ddd82a20f420b4
hmurillo66 <41769880+hmurillo66@users.noreply.github.com>
2018-08-04 12:32:50 -0600

    Update Lavacar.java

--- src/lavacar/Lavacar.java
+++ src/lavacar/Lavacar.java
@@ -17,7 +17,7 @@ public class Lavacar {
     public static void main(String[] args) {
         
         
-        //mostrar el login
+        
         Login n =new Login ();
         n.setVisible(true);
  
