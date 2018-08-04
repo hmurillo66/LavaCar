6d157e2897fa8737ff92b31812b35196c66250aa
hanse <hanse@Hansel>
2018-08-04 12:48:30 -0600

    Version 3

--- src/lavacar/CobrarOrden.java
+++ src/lavacar/CobrarOrden.java
@@ -180,6 +180,10 @@ public class CobrarOrden  extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        try {
+            
+        } catch (Exception e) {
+        }
         
     }//GEN-LAST:event_jButton1ActionPerformed
 
