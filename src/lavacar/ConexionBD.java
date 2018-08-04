31edf94b08bd01f99d5b0b84ad7fda8203274057
hanse <hanse@Hansel>
2018-08-04 12:06:24 -0600

    Version 1

---
+++ src/lavacar/ConexionBD.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lavacar;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author angie
+ */
+public class ConexionBD {
+    // creamos la variable tipo conexion y la declaramos con el nombre de conectar
+    Connection conectar =null;
+    // metodo tipo connectio para realizar la conexion
+    public Connection conexion(){
+        try {
+            // instancia
+            Class.forName("org.gjt.mm.mysql.Driver");
+            conectar=(Connection)DriverManager.getConnection("jdbc:mysql://localhost/sistema_lavacar","root","");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error de conexion de la base de datos");
+        } catch(ClassNotFoundException ex){
+        
+        }
+        return conectar;
+    
+    }
+}
---
+++ src/lavacar/Lavacar.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lavacar;
+
+/**
+ * 
+ * @author caleblopez
+ */
+public class Lavacar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Login n =new Login ();
+        n.setVisible(true);
+ 
+
+      
+    }
+    
+}
